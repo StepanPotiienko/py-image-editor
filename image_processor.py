23b8cfaa6c9f0d5a7c2ca5b624689946557d84ad
Stepan Potiienko <kotnexjieb@protonmail.com>
2024-10-25 12:57:05 +0300

    moved open and save methods to another file, added functionality to open new image in the app. Now app opens the last edited image etc

---
+++ image_processor.py
@@ -0,0 +1,33 @@
+import customtkinter as ctk
+from PIL import Image
+
+
+class ImageProcessor:
+    def open_image(self):
+        file_path = ctk.filedialog.askopenfilename(
+            defaultextension=".jpg",
+            filetypes=[
+                ("JPEG files", "*.jpg"),
+                ("PNG files", "*.png"),
+                ("All files", "*.*"),
+            ],
+        )
+
+        if file_path:
+            return file_path
+
+    def save_image(self, img: Image.Image):
+        file_path = ctk.filedialog.asksaveasfilename(
+            defaultextension=".jpg",
+            filetypes=[
+                ("JPEG files", "*.jpg"),
+                ("PNG files", "*.png"),
+                ("All files", "*.*"),
+            ],
+        )
+
+        if file_path:
+            with open("img_path.txt", "w") as file:
+                file.write(file_path)
+
+                img.save(file_path)
---
+++ todo.md
@@ -0,0 +1,7 @@
+### TODO
+* More options to enhance image. 
+* Improve UX by providing labels to each slider.
+* Fix performance issues while working with 4k+ images.
+* Add spaces between sliders and buttons, original image and button.
+* Add auto image resize for large images.
+* Write last file path to another directory.
